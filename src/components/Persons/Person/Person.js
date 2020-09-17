e25e3f4867861bda19f36b5ed52e77088cf6819c
geogeorgiou <georgiogeo27@gmail.com>
2020-09-17 23:13:36 +0300

    withClass cannot forward props for you by itself you need spread operator for that! (for styles just pass the className)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Aux from '../../../hoc/Auxiliary'
+import withClass from "../../../hoc/withClass";
 import classes from './Person.css'
 
 
@@ -19,11 +20,11 @@ class Person extends Component {
 
             // instead of <div className="Person" style={style}> use Aux hoc
             //INSTEAD of Aux hack you can use the built in React.Fragment or Fragment if you import Fragment
-            <React.Fragment>
+            <Aux>
                 <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p key="i2">{this.props.children}</p>
                 <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-            </React.Fragment>
+            </Aux>
 
         );
 
@@ -32,4 +33,4 @@ class Person extends Component {
 
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
